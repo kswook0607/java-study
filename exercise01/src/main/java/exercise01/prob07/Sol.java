00ac03c6f4cf48fe20ab4a89a478d31c716769f4
kswook0607 <kswook0607@gmail.com>
2025-07-18 20:20:40 +0900

    exercise01

--- exercise01/src/main/java/exercise01/prob07/Sol.java
+++ exercise01/src/main/java/exercise01/prob07/Sol.java
@@ -3,6 +3,17 @@ package exercise01.prob07;
 public class Sol {
 
 	public static void main(String[] args) {
+		
+		
+		for(int i = 1; i <= 9; i++) {
+			
+			for(int j=0; j < 10; j++) {
+				
+				System.out.print((i + j) + " ");
+				
+			}System.out.println();
+			
+		}
 
 
 	}
