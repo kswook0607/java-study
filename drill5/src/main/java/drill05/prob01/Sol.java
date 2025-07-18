cb022118b2d9f6920a70a0eb1685401556a95cf4
bit <bit@DESKTOP-VJKB5MJ>
2025-07-18 09:09:01 +0900

    drill05

---
+++ drill5/src/main/java/drill05/prob01/Sol.java
@@ -0,0 +1,14 @@
+package drill05.prob01;
+
+import java.util.Scanner;
+
+public class Sol {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		/* 코드 작성 */
+		
+		scanner.close();
+	}		
+}
