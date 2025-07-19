673d4a7a597cdbfd000e0aa673714ded81533206
kswook0607 <kswook0607@gmail.com>
2025-07-19 13:02:20 +0900

    exercise02

--- exercise02/src/main/java/exercise02/prob01/Sol.java
+++ exercise02/src/main/java/exercise02/prob01/Sol.java
@@ -13,20 +13,27 @@ public class Sol {
 		System.out.print("끝 섭씨: ");
 		int endC = scanner.nextInt();
 
-		System.out.print("증가 간: ");
+		System.out.print("증가 간격: ");
 		int step = scanner.nextInt();
 
 		scanner.close();
 
 		
+		System.out.println("C\tF");
 		/* 코드 작성 */
+		for(int i = startC; i <= endC; i+=step) {
+			int f = (i * 9 / 5) + 32;
+			System.out.println(i + "\t" + f);			
+			
+		}
+		
 				
 		
 		/* 출력 예시 */
-		System.out.println("C\tF");
-		System.out.println("0\t32.0");
-		System.out.println("5\t41.0");
-		System.out.println("10\t41.0");		
+//		System.out.println("C\tF");
+//		System.out.println("0\t32.0");
+//		System.out.println("5\t41.0");
+//		System.out.println("10\t41.0");		
 	}
 
 }
--- exercise02/src/main/java/exercise02/prob02/Sol.java
+++ exercise02/src/main/java/exercise02/prob02/Sol.java
@@ -7,9 +7,37 @@ public class Sol {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int[] numbers = new int[10];
-
+	
 		System.out.print("입력: ");
 
+		for(int i = 0; i < numbers.length; i++) {
+			
+			numbers[i]= scanner.nextInt();
+		}
+		
+		int max = numbers[0];
+		int min = numbers[0];
+		
+		for(int i = 1; i < numbers.length; i++ ) {
+			
+			if(numbers[i] > max) {
+				
+				max = numbers[i];				
+				
+			}
+			
+			if(numbers[i] < min ) {
+				
+				min = numbers[i];
+			}
+		}
+				
+		int sub = max - min;
+		
+		System.out.println("최대-최소 차이: " + sub);
+		
+		
+		
 		/* 코드 작성 */
 
 		scanner.close();		
